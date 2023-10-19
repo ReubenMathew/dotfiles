0c79a0873fbb56f61a8c396913c9503579af8d5c
reubenninan <intelphysic@gmail.com>
2023-10-19 13:33:44 -0400

    Formatting and code action plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,24 +1,24 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 local uv = vim.uv or vim.loop
 
 -- Auto-install lazy.nvim if not present
 if not uv.fs_stat(lazypath) then
-	print('Installing lazy.nvim....')
+	print("Installing lazy.nvim....")
 	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
 		lazypath,
 	})
-	print('Done.')
+	print("Done.")
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- lazy packages
 require("lazy").setup({
-	{ 'rose-pine/neovim',       name = 'rose-pine' },
+	{ "rose-pine/neovim",       name = "rose-pine" },
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
@@ -31,41 +31,41 @@ require("lazy").setup({
 				highlight = { enable = true },
 				indent = { enable = true },
 			})
-		end
+		end,
 	},
 	-- LSP Support
 	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
 		lazy = true,
 		config = false,
 	},
 	{
-		'neovim/nvim-lspconfig',
+		"neovim/nvim-lspconfig",
 		dependencies = {
-			{ 'hrsh7th/cmp-nvim-lsp' },
-		}
+			{ "hrsh7th/cmp-nvim-lsp" },
+		},
 	},
 	-- Autocompletion
 	{
-		'hrsh7th/nvim-cmp',
+		"hrsh7th/nvim-cmp",
 		dependencies = {
-			{ 'L3MON4D3/LuaSnip' }
+			{ "L3MON4D3/LuaSnip" },
 		},
 	},
 	-- Telescope
 	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.4',
-		dependencies = { 'nvim-lua/plenary.nvim' }
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	-- Utilities
-	{ 'preservim/nerdcommenter' },
+	{ "preservim/nerdcommenter" },
 	{
 		"folke/trouble.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
-	{ "jose-elias-alvarez/null-ls.nvim", },
+	{ "jose-elias-alvarez/null-ls.nvim" },
 	{
 		"nvim-tree/nvim-tree.lua",
 		version = "*",
@@ -74,91 +74,111 @@ require("lazy").setup({
 			"nvim-tree/nvim-web-devicons",
 		},
 		config = function()
-			require("nvim-tree").setup {}
+			require("nvim-tree").setup({})
 		end,
 	},
 	{ "lewis6991/gitsigns.nvim" },
 	{ "nvim-treesitter/nvim-treesitter-context" },
+	{ "rcarriga/nvim-notify" },
 	{
-	  'nvim-lualine/lualine.nvim',
-	  event = 'ColorScheme',
-	  config = function()
-		require('lualine').setup({
-		  options = {
-			--- @usage 'rose-pine' | 'rose-pine-alt'
-			theme = 'rose-pine'
-		  }
-		})
-	  end
+		"nvim-lualine/lualine.nvim",
+		event = "ColorScheme",
+		config = function()
+			require("lualine").setup({
+				options = {
+					--- @usage 'rose-pine' | 'rose-pine-alt'
+					theme = "rose-pine",
+				},
+			})
+		end,
 	},
-	{ 'github/copilot.vim' },
+	{ "github/copilot.vim" },
 	{
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
-	{ 'mfussenegger/nvim-dap' },
-	{ 'leoluz/nvim-dap-go' },
+	{ "mfussenegger/nvim-dap" },
+	{ "leoluz/nvim-dap-go" },
+	{
+		"ThePrimeagen/refactoring.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("refactoring").setup()
+		end,
+	},
+	{
+		"stevearc/dressing.nvim",
+		opts = {},
+	},
 })
 
+-- Nvim Notify
+vim.notify = require("notify")
+
 -- DAP Debugger
-require('dap-go').setup()
+require("dap-go").setup()
 
 -- todo-comments
-require('todo-comments').setup()
+require("todo-comments").setup()
 
 -- lua_line
-require('lualine').setup {
-  options = {
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
-    },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = { 'diagnostics', 'diff' },
-    lualine_x = {},
-    lualine_y = { 'filetype', 'fileformat', 'progress' },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
-  },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-  tabline = {},
-  extensions = {},
-}
+require("lualine").setup({
+	options = {
+		component_separators = "|",
+		section_separators = { left = "", right = "" },
+	},
+	sections = {
+		lualine_a = {
+			{ "mode", separator = { left = "" }, right_padding = 2 },
+		},
+		lualine_b = { "filename", "branch" },
+		lualine_c = { "diagnostics", "diff" },
+		lualine_x = {},
+		lualine_y = { "filetype", "fileformat", "progress" },
+		lualine_z = {
+			{ "location", separator = { right = "" }, left_padding = 2 },
+		},
+	},
+	inactive_sections = {
+		lualine_a = { "filename" },
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = { "location" },
+	},
+	tabline = {},
+	extensions = {},
+})
 
 -- gitsigns
-require('gitsigns').setup()
+require("gitsigns").setup()
 
 -- null-ls
 local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.completion.spell,
-		null_ls.builtins.code_actions.impl,
-		null_ls.builtins.code_actions.gomodifytags,
-		null_ls.builtins.diagnostics.shellcheck,
 		null_ls.builtins.formatting.rustfmt,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.gofmt,
+		null_ls.builtins.code_actions.impl,
+		null_ls.builtins.code_actions.refactoring,
+		null_ls.builtins.code_actions.gomodifytags,
+		null_ls.builtins.completion.spell,
+		null_ls.builtins.diagnostics.shellcheck,
 	},
 })
 
 -- Colorscheme
 --- Config
-require('rose-pine').setup({
+require("rose-pine").setup({
 	--- @usage 'auto'|'main'|'moon'|'dawn'
-	variant = 'auto',
+	variant = "auto",
 	--- @usage 'main'|'moon'|'dawn'
-	dark_variant = 'main',
+	dark_variant = "main",
 	bold_vert_split = false,
 	dim_nc_background = false,
 	disable_background = true,
@@ -177,7 +197,7 @@ require('rose-pine').setup({
 	},
 })
 --- Set colorscheme after options
-vim.cmd('colorscheme rose-pine')
+vim.cmd("colorscheme rose-pine")
 
 -- VIM OPTIONS
 --- Tab Sizing
@@ -204,79 +224,96 @@ vim.keymap.set("o", "<S-Tab>", "<ESC>", {})
 --- ; same as :
 vim.keymap.set("n", ";", ":", {})
 --- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<c-p>', builtin.find_files, {})
-vim.keymap.set('n', '<c-g>', builtin.git_files, {})
-vim.keymap.set('n', '<c-f>', builtin.live_grep, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<c-p>", builtin.find_files, {})
+vim.keymap.set("n", "<c-g>", builtin.git_files, {})
+vim.keymap.set("n", "<c-f>", builtin.live_grep, {})
 --- NvimTree
 --local api = require("nvim-tree.api")
-vim.keymap.set("n", "<leader>t", ":NvimTreeFindFileToggle<CR>", {silent = true})
+vim.keymap.set("n", "<leader>t", ":NvimTreeFindFileToggle<CR>", { silent = true })
 
 -- LSP
 --- Setup
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 lsp_zero.on_attach(function(client, bufnr)
 	-- see :help lsp-zero-keybindings
 	-- to learn the available actions
 	lsp_zero.default_keymaps({ buffer = bufnr })
-	vim.keymap.set({ 'n', 'v' }, '<leader>a', vim.lsp.buf.code_action, {buffer = bufnr})
+	vim.keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, { buffer = bufnr })
+	vim.keymap.set({ "n" }, "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr })
 end)
-
+--- Autocmds
+vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
 --- Language Servers
-require('lspconfig').tsserver.setup({})
-require('lspconfig').rust_analyzer.setup({})
-require('lspconfig').gopls.setup({})
-require('lspconfig').bashls.setup({})
-require('lspconfig').jdtls.setup({})
-require 'lspconfig'.lua_ls.setup {
+require("lspconfig").tsserver.setup({})
+require("lspconfig").rust_analyzer.setup({})
+require("lspconfig").gopls.setup({
+	cmd = { "gopls" },
+	settings = {
+		gopls = {
+			experimentalPostfixCompletions = true,
+			analyses = {
+				unusedparams = true,
+				shadow = true,
+			},
+			staticcheck = true,
+		},
+	},
+	init_options = {
+		usePlaceholders = true,
+	},
+})
+require("lspconfig").bashls.setup({})
+require("lspconfig").jdtls.setup({})
+require("lspconfig").lua_ls.setup({
 	on_init = function(client)
 		local path = client.workspace_folders[1].name
-		if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-			client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+		if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+			client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
 				Lua = {
 					runtime = {
 						-- Tell the language server which version of Lua you're using
 						-- (most likely LuaJIT in the case of Neovim)
-						version = 'LuaJIT'
+						version = "LuaJIT",
 					},
 					-- Make the server aware of Neovim runtime files
 					workspace = {
 						checkThirdParty = false,
 						library = {
-							vim.env.VIMRUNTIME
+							vim.env.VIMRUNTIME,
 							-- "${3rd}/luv/library"
 							-- "${3rd}/busted/library",
-						}
+						},
 						-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
 						-- library = vim.api.nvim_get_runtime_file("", true)
-					}
-				}
+					},
+				},
 			})
 
 			client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
 		end
 		return true
-	end
-}
+	end,
+})
 
 -- Autocompletion
-local cmp = require('cmp')
+local cmp = require("cmp")
 local cmp_action = lsp_zero.cmp_action()
 
 cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		-- `Enter` key to confirm completion
-		['<CR>'] = cmp.mapping.confirm({ select = false }),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
 
 		-- Ctrl+Space to trigger completion menu
-		['<C-Space>'] = cmp.mapping.complete(),
+		["<C-Space>"] = cmp.mapping.complete(),
 
 		-- Navigate between snippet placeholder
-		['<C-f>'] = cmp_action.luasnip_jump_forward(),
-		['<C-b>'] = cmp_action.luasnip_jump_backward(),
+		["<C-f>"] = cmp_action.luasnip_jump_forward(),
+		["<C-b>"] = cmp_action.luasnip_jump_backward(),
 
 		-- Scroll up and down in the completion documentation
-		['<C-u>'] = cmp.mapping.scroll_docs(-4),
-		['<C-d>'] = cmp.mapping.scroll_docs(4),
-	})
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
+	}),
 })
