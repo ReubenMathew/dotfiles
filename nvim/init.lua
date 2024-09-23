476f088244e7fe0ee9d611fc0e6e51b882ea4f01
reubenninan <intelphysic@gmail.com>
2024-09-23 20:43:40 +0000

    fixes and new stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,7 +18,7 @@ vim.opt.rtp:prepend(lazypath)
 
 -- lazy packages
 require("lazy").setup({
-	{ "rose-pine/neovim",       name = "rose-pine" },
+	{ "rose-pine/neovim",            name = "rose-pine" },
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
@@ -45,12 +45,23 @@ require("lazy").setup({
 			})
 		end,
 	},
+	-- Snippets
+	{ "rafamadriz/friendly-snippets" },
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		--build = "make install_jsregexp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+	},
+	{ "saadparwaiz1/cmp_luasnip" },
 	-- LSP Support
 	{
 		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
-		lazy = true,
-		config = false,
+		branch = "v4.x",
+		--lazy = true,
+		--config = false,
 	},
 	{
 		"neovim/nvim-lspconfig",
@@ -58,19 +69,25 @@ require("lazy").setup({
 			{ "hrsh7th/cmp-nvim-lsp" },
 		},
 	},
-	-- Autocompletion
+	--- Autocompletion
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
 			{ "L3MON4D3/LuaSnip" },
 		},
+		sources = {
+			name = "luasnip",
+			option = {
+				use_show_condition = false,
+				show_autosnippets = true,
+			},
+		},
 	},
 	{
 		"hrsh7th/cmp-path",
 	},
-	{
-		"hrsh7th/cmp-cmdline",
-	},
+	{ "hrsh7th/cmp-nvim-lsp-signature-help" },
+	{ "onsails/lspkind.nvim" },
 	-- Telescope
 	{
 		"nvim-telescope/telescope.nvim",
@@ -81,7 +98,15 @@ require("lazy").setup({
 	{ "preservim/nerdcommenter" },
 	{
 		"folke/trouble.nvim",
+		cmd = "Trouble",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			win = {
+				wo = {
+					wrap = true,
+				},
+			},
+		},
 	},
 	{ "jose-elias-alvarez/null-ls.nvim" },
 	{
@@ -124,6 +149,23 @@ require("lazy").setup({
 			"nvim-lua/plenary.nvim",
 			"nvim-treesitter/nvim-treesitter",
 		},
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	{
+		"rhysd/vim-fixjson",
+		lazy = true,
+	},
+	{
+		"sindrets/diffview.nvim",
 	},
 })
 
@@ -210,22 +252,30 @@ require("rose-pine").setup({
 vim.cmd.colorscheme("rose-pine")
 
 -- VIM OPTIONS
+--- Swap File
+vim.o.swapfile = false
 --- Tab Sizing
 vim.o.tabstop = 4
 vim.o.shiftwidth = vim.o.tabstop
 --- Options
-vim.o.nocompatible = true
+--vim.o.nocompatible = true
 vim.o.wildmenu = true
 vim.o.autoindent = true
 vim.o.shell = "/bin/bash"
 vim.o.number = true
 vim.o.clipboard = "unnamed"
 vim.o.termguicolors = true
+vim.o.scrolloff = 8
+vim.o.signcolumn = "yes"
+vim.o.updatetime = 50
 -- Status Line
-vim.opt.laststatus = 3 -- Or 3 for global statusline
-vim.opt.statusline = " %f %m %= %l:%c "
+vim.o.laststatus = 3 -- Or 3 for global statusline
+vim.o.statusline = " %f %m %= %l:%c "
 -- Copilot
 vim.g.copilot_enabled = false
+-- Undo
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.o.undofile = true
 
 -- MAPPINGS
 --- Remap escape
@@ -241,47 +291,57 @@ vim.keymap.set("n", "<c-f>", builtin.live_grep, {})
 --- NvimTree
 vim.keymap.set("n", "<leader>t", ":NvimTreeFindFileToggle<CR>", { silent = true })
 --- Trouble
-vim.keymap.set("n", "<c-t>", ":TroubleToggle<CR>", { silent = true })
+vim.keymap.set("n", "<c-t>", ":Trouble diagnostics toggle<CR>", { silent = true })
 --- Toggle light/dark mode
 vim.keymap.set("n", "<leader>l", ":lua toggle_theme()<CR>", { silent = true })
 
 -- LSP
 --- Setup
 local lsp_zero = require("lsp-zero")
-lsp_zero.on_attach(function(client, bufnr)
+local lsp_attach = function(client, bufnr)
 	-- see :help lsp-zero-keybindings
 	-- to learn the available actions
 	lsp_zero.default_keymaps({ buffer = bufnr })
 	vim.keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, { buffer = bufnr })
 	vim.keymap.set({ "n", "v", "i" }, "<c-s>", vim.lsp.buf.format, { buffer = bufnr })
 	vim.keymap.set({ "n" }, "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr })
-end)
+end
+lsp_zero.extend_lspconfig({
+	capabilities = require("cmp_nvim_lsp").default_capabilities(),
+	lsp_attach = lsp_attach,
+	float_border = "rounded",
+	sign_text = true,
+})
 --- Language Servers
 require("lspconfig").terraformls.setup({})
 require("lspconfig").jedi_language_server.setup({})
 require("lspconfig").yamlls.setup({})
 require("lspconfig").bashls.setup({})
 require("lspconfig").html.setup({})
-require("lspconfig").tsserver.setup({})
+require("lspconfig").ts_ls.setup({})
 require("lspconfig").denols.setup({})
 require("lspconfig").rust_analyzer.setup({})
+require("lspconfig").jdtls.setup({})
 require("lspconfig").gopls.setup({
 	cmd = { "gopls" },
+	filetypes = { "go", "gomod", "gowork", "gotmpl", "tmpl" },
 	settings = {
 		gopls = {
+			completeUnimported = true,
+			usePlaceholders = true,
 			experimentalPostfixCompletions = true,
 			analyses = {
 				unusedparams = true,
 				shadow = true,
 			},
 			staticcheck = true,
+			gofumpt = true,
 		},
 	},
 	init_options = {
 		usePlaceholders = true,
 	},
 })
-require("lspconfig").jdtls.setup({})
 require("lspconfig").lua_ls.setup({
 	on_init = function(client)
 		local path = client.workspace_folders[1].name
@@ -298,7 +358,7 @@ require("lspconfig").lua_ls.setup({
 						checkThirdParty = false,
 						library = {
 							vim.env.VIMRUNTIME,
-							-- "${3rd}/luv/library"
+							"${3rd}/luv/library",
 							-- "${3rd}/busted/library",
 						},
 						-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
@@ -322,16 +382,41 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 		vim.lsp.buf.format()
 	end,
 })
+--- Show line diagnostics automatically in hover window
+vim.diagnostic.config({
+	virtual_text = false,
+})
+--vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
 
 -- Autocompletion
 local cmp = require("cmp")
 local cmp_action = lsp_zero.cmp_action()
+local lspkind = require("lspkind")
+
+--- default sources for all buffers
+local default_cmp_sources = cmp.config.sources({
+	{ name = "nvim_lsp" },
+	{ name = "nvim_lsp_signature_help" },
+}, {
+	{ name = "luasnip" },
+	{ name = "path" },
+})
 
 cmp.setup({
+	preselect = "item",
+	completion = {
+		completeopt = "menu,menuone,noinsert",
+	},
 	sources = {
 		{ name = "nvim_lsp" },
 		{ name = "path" },
-		{ name = "cmdline" },
+		{ name = "luasnip" },
+		{ name = "nvim_lsp_signature_help" },
+	},
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
 	},
 	mapping = cmp.mapping.preset.insert({
 		-- `Enter` key to confirm completion
@@ -347,7 +432,50 @@ cmp.setup({
 		-- Scroll up and down in the completion documentation
 		["<C-u>"] = cmp.mapping.scroll_docs(-4),
 		["<C-d>"] = cmp.mapping.scroll_docs(4),
+
+		-- Complete on tab
+		["<Tab>"] = cmp.mapping.confirm({ select = false }),
 	}),
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol", -- show only symbol annotations
+			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+			-- can also be a function to dynamically calculate max width such as
+			-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+			show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+			-- The function below will be called before any actual modifications from lspkind
+			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+			--before = function (entry, vim_item)
+			--...
+			--return vim_item
+			--end
+		}),
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	enabled = function()
+		-- disable completion in comments
+		local context = require("cmp.config.context")
+		-- keep command mode completion enabled when cursor is in a comment
+		if vim.api.nvim_get_mode().mode == "c" then
+			return true
+		else
+			return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+		end
+	end,
+})
+
+-- Snippets
+require("luasnip.loaders.from_vscode").lazy_load({
+	include = {
+		"go",
+		"lua",
+		"sh",
+	},
 })
 
 -- Custom Functions
